e32dab7cf175c44ced4463cf6a031bd0672c8992
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2021-03-15 18:00:10 +0900

    first commit

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'sg=op2bzln^tegq_=3i&aj+ec*fv(ito#+o^1+8eaobd3m^1an'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DATABASE 명',
+        'USER': 'root',
+        'PASSWORD': 'DB접속용 비밀번호',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
