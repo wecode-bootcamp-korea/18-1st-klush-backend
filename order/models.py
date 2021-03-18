4a324ec5326290ee1b6a1e57a486097d62794a76
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2021-03-18 17:41:20 +0900

    ADD: order table column  null=True added

--- order/models.py
+++ order/models.py
@@ -8,9 +8,9 @@ class Order(models.Model):
     created_at   = models.DateTimeField(auto_now_add=True)
     updated_at   = models.DateTimeField(auto_add=True)
     total_price  = models.DecimalField(max_digits=10, decimal_places=2)
-    payment      = models.OneToOneField('Payment', on_delete=models.CASCADE)
+    payment      = models.OneToOneField('Payment', on_delete=models.CASCADE, null=True)
     user         = models.ForeignKey('user.User', on_delete=models.CASCADE)
-    address      = models.ForeignKey('Address', on_delete=models.CASCADE)
+    address      = models.ForeignKey('Address', on_delete=models.CASCADE, null=True)
     order_status = models.ForeignKey('OrderStatus', on_delete=models.CASCADE)
 
     class Meta:
