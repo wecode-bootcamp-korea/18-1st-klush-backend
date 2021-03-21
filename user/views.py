c87ac342667594e6d29a7847927e5cf8c5cf94e3
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2021-03-21 18:57:32 +0900

    MODIFIED: user/views&utils/decorator exception handling,convention,return message

--- user/views.py
+++ user/views.py
@@ -15,33 +15,30 @@ class SignInView(View):
             data     = json.loads(request.body)
             email    = data.get('email')
             password = data['password']
-            
 
             regex_email    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
             regex_password = '^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{10,25}'
 
             if not re.match(regex_email,email):
-                return JsonResponse({'message':'잘못된 형식입니다. 이메일을 다시 한번 확인해 주세요'}, status=400)
+                return JsonResponse({'message':'INVALID_EMAIL'}, status=401)
 
             if not re.match(regex_password,password):
-                return JsonResponse({'message':'비밀번호는 10자리 이상, 영문과 숫자, 특수문자를 모두 입력해주세요.'})
+                return JsonResponse({'message':'INVALID_PASSWORD'},status=401)
 
-            user = User.objects.get(email=email)
+            if not User.objects.filter(email=email).exist():
+                return JsonResponse({'message':'INVALID_USER'}, status=401)
 
             if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
-                return JsonResponse({'message':'유효하지 않은 비밀번호 입니다.'}, status=401)
+                return JsonResponse({'message':'INVALID_USER'}, status=401)
 
             access_token = jwt.encode({'user_id': user.id}, SECRET_KEY, ALGORITHM)
-            return JsonResponse({'token': access_token, 'name': user.name, 'message':'로그인 되었습니다.'}, status=200)
+            return JsonResponse({'token': access_token, 'name': user.name, 'message':'SUCCESS'}, status=200)
 
         except json.JSONDecodeError:
             return JsonResponse({'message': 'JSONDECODE_ERROR'}, status=400)
 
-        except User.DoesNotExist:
-            return JsonResponse({'message':'INVALID_USER'}, status=401)
-
         except User.MultipleObjectsReturned:
-            return JsonResponse({'message':'INVALID_USER, get() returned more than one -- it returned 2!'}, status=401)
+            return JsonResponse({'message':'INVALID_USER'}, status=401)
 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
--- utils/decorator.py
+++ utils/decorator.py
@@ -9,14 +9,11 @@ from my_settings import SECRET_KEY, ALGORITHM
 def authorization (func):
     def wrapper (self, request, *args, **kwrgs):
         try:
-            token = request.headers['Authorization']
+            token         = request.headers['Authorization']
             decoded_token = jwt.decode(token, SECRET_KEY, ALGORITHM)
-            request.user = User.objects.get(id=decoded_token["user_id"])
+            request.user  = User.objects.get(id=decoded_token["user_id"])
             return func(self, request, *args, **kwargs)
             
-        except ValueError:
-            return JsonResponse({'message':'It returned None instead.'}, status=400)
-
         except jwt.DecodeError:
-            return JsonResponse({'message': 'Not exact token'}, status=400)
+            return JsonResponse({'message': 'Not exact token'}, status=401)
     return wrapper
