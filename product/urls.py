99cfe4cd826f4a118eb7bcb731784c4a9b239126
jadyJeong <coldfeet0816@gmail.com>
2021-03-23 20:37:49 +0900

    ADD: main page view created, WIP

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
-urlpatterns = [
+from .views import MainView
 
+urlpatterns = [
+    path('/main',MainView.as_view())
 ]
---
+++ product/views.py
@@ -0,0 +1,28 @@
+import json
+from django.http     import JsonResponse
+
+from django.views    import View
+from product.models  import Menu, Category, Product, Image
+
+#메인 뿌려주는 뷰. url은 /main
+class MainView(View):
+    def get(self,request):
+
+        targets = Product.objects.filter(is_new=1)
+        new_product_list = []
+        
+        # is_new 인 애들을 가져옴
+        for target in targets:
+            new_product_list.append(
+                {
+                    "image_url" : [img.image_url for img in target.image_set.all()],
+                    "name"      : target.name,
+                    # "label"     :target.label,
+                    "price"     : target.price
+                }
+            )
+            
+        print (new_product_list)
+        return JsonResponse({'new_product_list':new_product_list},status=200)
+
+        
