cd4e30ab40537f9dfa9c82d405d00226d48edca3
ziy0ung1234 <dnjsejrjfsm1@gmail.com>
2021-03-24 18:42:47 +0900

    inprogress

--- order/urls.py
+++ order/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .views      import OrderProductView
 
 urlpatterns = [
-    path('/cart/<int:product_id>', OrderProductView.as_view())
+    path('/cart/<int:product_id>', OrderProductView.as_view()),
+    path('/cart-list', OrderProductView.as_view())
+
 ]
--- order/views.py
+++ order/views.py
@@ -1,5 +1,5 @@
 import json
-import random
+import uuid
 
 from django.http      import JsonResponse
 from django.views     import View
@@ -17,25 +17,21 @@ class OrderProductView(View):
             data         = json.loads(request.body)
             user_id      = request.user.id
             quantity     = data['quantity']
-            # product_id   = Product.objects.get(id=product_id) #등록할 상품의 정보 get으로 받음
             order_status = OrderStatus.objects.get(id=1) #status code 1 = 결제전
 
             if not Order.objects.filter(user_id=user_id, order_status=1).exists():
 
-                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
             # 유저정보 일치, status 1번인 오더가 없을 때 새로 생성
                 order = Order.objects.create(
-                    order_number = ,
+                    order_number = uuid.uuid4(),
                     user_id = user_id,
                     order_status = order_status,
                 )
-                print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
                 OrderProduct.objects.create(
                     total_quantity = quantity,
                     product_id     = product_id,    
                     order_id       = order.id,
                 )
-                print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
                 return JsonResponse({'message':'SUCCESS'}, status=201)
 
             order = Order.objects.get(user_id=user_id, order_status=1)
@@ -48,7 +44,7 @@ class OrderProductView(View):
                 return JsonResponse({'message':'SUCCESS_CREATE'}, status=201)
 
             order_product = OrderProduct.objects.get(order=order, product_id=product_id)
-            order_product.quantity += quantity
+            order_product.total_quantity += quantity
             order_product.save()
 
             return JsonResponse({'message':'SUCCESS'}, status=200)
@@ -58,3 +54,24 @@ class OrderProductView(View):
 
         except json.decoder.JSONDecodeError:
             return JsonResponse({'MESSAGE': 'JSON_DECODE_ERROR'}, status=400)
+
+    def get (self, request):
+        try:
+            user_id = request.GET['user_id']
+            print(user_id)
+            order = Orders.objects.filter(user=user, order_status=1)
+            cart_lists = order.orderproduct_set.all()
+            
+            cart_list = [{
+                "id": carts.id,
+                "sub_category_name": Product.sub_category.name,
+                "price": carts.product.price,
+                "product_name": carts.product.name,
+                "quantity": carts.total_quantity,
+                "image_url": [i.image_url for i in Image.objects.filter(product_id = product.id)],
+            } for car_list in cart_lists]
+
+            return JsonResponse({'message':'SUCCESS', 'cart':cart_list},status=200)
+
+        except OrderProduct.DoesNotExist:
+            return JsonResponse({"MESSAGE": "DOES_NOT_EXIST", "items_in_cart":[]}, status=400)
