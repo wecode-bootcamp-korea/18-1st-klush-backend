70e0a08b62553be9c5712aa62288c5c0a05a17e8
InahChoi <hyejooworld@gmail.com>
2021-03-24 16:37:03 +0900

    UPDATE : ProductListView

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls   import path
+
 from .views        import ProductListView, SubCategoryView
 
 urlpatterns = [
     path('', ProductListView.as_view()),
-    path('test', SubCategoryView.as_view())
+    path('/category', SubCategoryView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -2,7 +2,7 @@ import json
 from django.http        import JsonResponse
 
 from django.views       import View
-from product.models     import Product, Image, SubCategory
+from product.models     import SubCategory, Product
 
 class SubCategoryView(View):
     def get(self, request):
@@ -17,14 +17,14 @@ class ProductListView(View):
     def get(self, request):
         products     = Product.objects.all()
         product_list = [{
+            'product_id'      : product.id,
             'sub_category'    : product.sub_category.name,
             'name'            : product.name,
-            'images_url'      : [i.image_url for i in Image.objects.filter(product_id = product.id)],
+            'images'          : [image.image_url for image in product.image_set.all()],
             'price'           : product.price,
             'is_vegan'        : product.is_vegan,
             'is_new'          : product.is_new,
             'is_soldout'      : product.is_soldout,
-            'product_labels'  : [l.label.name for l in product.productlabel_set.all()],
-            'product_id'      : product.id,
+            'product_labels'  : [label.label.name for label in product.productlabel_set.all()],
         } for product in products]
         return JsonResponse({'product_list_data' : product_list}, status = 200)
