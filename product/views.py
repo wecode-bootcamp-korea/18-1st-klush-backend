717adcfefdf9fb2532526e8044e9a18005bb1453
jadyJeong <coldfeet0816@gmail.com>
2021-03-24 11:27:37 +0900

    UPDATE: handling exceptions WIP.

--- product/views.py
+++ product/views.py
@@ -7,22 +7,27 @@ from product.models  import Menu, Category, Product, Image
 #메인 뿌려주는 뷰. url은 /main
 class MainView(View):
     def get(self,request):
-
-        targets = Product.objects.filter(is_new=1)
-        new_product_list = []
-        
-        # is_new 인 애들을 가져옴
-        for target in targets:
-            new_product_list.append(
-                {
-                    "image_url" : [img.image_url for img in target.image_set.all()],
-                    "name"      : target.name,
-                    # "label"     :target.label,
-                    "price"     : target.price
-                }
-            )
+        try:
+            targets = Product.objects.filter(is_new=1)
+            new_product_list = []
             
-        print (new_product_list)
-        return JsonResponse({'new_product_list':new_product_list},status=200)
+            # is_new 인 애들을 가져옴
+            for target in targets:
+                new_product_list.append(
+                    {
+                        "image_url" : [img.image_url for img in target.image_set.all()],
+                        "name"      : target.name,
+                        # "label"     :target.label,
+                        "price"     : target.price
+                    }
+                )
+                
+            print (new_product_list)
+            return JsonResponse({'new_product_list':new_product_list},status=200)
+        
+        except Product.DoesNotExist:
+            return JsonResponse({'Message':'DoesNotExist',status=401})
+            
+
 
         
